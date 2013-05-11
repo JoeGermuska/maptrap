ef09d2ea50f1e0c3cbedf73e52d63ed72f1f0d01
Joe Germuska <joe@germuska.com>
2013-05-11 17:33:29 -0500

    add some scraps of UI

--- core/views.py
+++ core/views.py
@@ -2,3 +2,7 @@
 from django.shortcuts import render
 def index(request):
     return render(request, 'index.html',{})
+    
+def generate_workbook(request):
+    """Given some input, return an Excel spreadsheet"""
+    pass
